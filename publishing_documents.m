4dabd35efce349e0ffd46ebf91a473b027865078
Ash Srinivas <ash.v.srinivas@gmail.com>
2016-01-04 18:52:04 -0600

    Added an example on how to publish documents of Matlab scripts with some basic LaTeX

---
+++ publishing_documents.m
@@ -0,0 +1,11 @@
+%% My Document to Publish
+
+%% Section 1
+% *This* is $x^2+e^{\pi i}$ _plotted_. 
+% <http://www.mathworks.com MathWorks>
+%
+% $e^{\pi i} + 1 = 0$
+
+x = linspace(-10, 10);
+y = x.^2+exp(pi*1i);
+plot(x, y);
