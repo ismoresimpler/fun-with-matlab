1c76d73d2ca6786fa38cd6877cdbfe5bf25b7299
Ash Srinivas <ash.v.srinivas@gmail.com>
2016-01-04 19:05:20 -0600

    Added more LaTeX capabilities via MuPAD and symbolic toolbox

--- publishing_documents.m
+++ publishing_documents.m
@@ -5,6 +5,20 @@
 % <http://www.mathworks.com MathWorks>
 %
 % $e^{\pi i} + 1 = 0$
+%
+% Add in an mathematical formula with LaTeX from MuPAD
+% 
+% $10\, x + x^x + 16 = 0$
+%
+% Add in a mathematical formula using the 
+% symbolic toolbox by typing in the following:
+% 
+% *syms x* 
+% 
+% *latex(x^2+10*x+16==0)*
+%
+% $x^2 + 10\, x + 16 = 0$
+
 
 x = linspace(-10, 10);
 y = x.^2+exp(pi*1i);
