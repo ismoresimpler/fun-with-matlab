a4870dbe4ae8729985e87fdd56522d6911b09a96
Ash Srinivas <ash.v.srinivas@gmail.com>
2016-01-05 15:02:15 -0600

    Added an enumeration to track the state of SmartAccount objects

---
+++ AccountState.m
@@ -0,0 +1,7 @@
+% create an enumeration class
+classdef AccountState
+    % add an enumeration block
+    enumeration
+        Open, Closed
+    end
+end
--- SmartAccount.m
+++ SmartAccount.m
@@ -1,13 +1,22 @@
 % create a class that inherits from Account
 classdef SmartAccount < Account
+    % create a properties block
+    properties
+        State = AccountState.Open;
+    end
     
     % create a methods block
     methods
         % create a function that allows
         % us to close an account
         function Close(obj)
-            obj.Withdraw(obj.Balance);
-            disp('account closed');
+            if obj.State == AccountState.Closed
+                disp('Error: account already closed.');
+            else
+                obj.Withdraw(obj.Balance);
+                disp('account closed');
+                obj.State = AccountState.Closed;
+            end
         end
     end
 end
